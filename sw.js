b4887b245fadf1fdba7000c662b580e3b32b5b36
ipota <kiratoki512810@gmail.com>
2025-09-24 10:06:55 +0900

    pwa6

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const urlsToCache = ["ipotazero.github.io/With-Yignwi/"]
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache)
+            return cache.addAll(urlsToCache.map((url) => new Request(url, { credentials: "same-origin" })))
         }),
     )
 })
