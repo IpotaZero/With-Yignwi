69cd55898b15cf96e9f249aff0ce75a0bfd00243
ipota <kiratoki512810@gmail.com>
2025-09-24 10:03:37 +0900

    pwa5

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "pwa-sample-caches"
-const urlsToCache = ["mako5656.github.io/pwa/", "mako5656.github.io/pwa/app.js"]
+const urlsToCache = ["ipotazero.github.io/With-Yignwi/"]
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(urlsToCache)
         }),
     )
@@ -11,8 +11,8 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response ? response : fetch(event.request)
+        caches.match(event.request).then((response) => {
+            return response ?? fetch(event.request)
         }),
     )
 })
